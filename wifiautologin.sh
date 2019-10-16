2ba7926a9e63877fb02445117b2a3db96362be78
manke <mankelu5@gmail.com>
2019-10-17 00:24:16 +0800

    更新

--- wifiautologin.sh
+++ wifiautologin.sh
@@ -7,7 +7,7 @@ username="这里输入账号"
 password="这里输入密码"
 while :
 do
-    if [ $(ping -c 3 -i 1 8.8.8.8| grep -c ttl)  -eq 0 ]
+    if [ $(ping -c 3 -i 3 www.baidu.com| grep -c ttl)  -eq 0 ]
         then
             curl 'https://u.njtech.edu.cn/cas/login?service=https://u.njtech.edu.cn/oauth2/authorize?client_id=Oe7wtp9CAMW0FVygUasZ&response_type=code&state=njtech' -c "cookie" -H 'Accept: */*' -H 'Accept-Language: zh-cn' -H 'User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' > tmp
             LT=$(grep "\"lt\"" tmp | head -n 1 | awk -F \" '{print $6}')
@@ -18,4 +18,5 @@ do
             DATA2="Cookie: JSESSIONID="$JSESSIONID"; insert_cookie="$INSERTCOOKIE
             curl 'https://u.njtech.edu.cn/cas/login?service=https%3A%2F%2Fu.njtech.edu.cn%2Foauth2%2Fauthorize%3Fclient_id%3DOe7wtp9CAMW0FVygUasZ%26response_type%3Dcode%26state%3Dnjtech' -H 'Accept: */*' -H 'Accept-Language: zh-cn' -H 'User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' -H "$DATA2"  --data "$DATA" --compressed ;
     fi
+    sleep 30
 done
