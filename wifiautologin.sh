94584507fb85df3a7408bd7e8f8671fd33b7b8b1
dingyang666 <46430061+dingyang666@users.noreply.github.com>
2020-01-08 01:50:47 +0800

    Update wifiautologin.sh

--- wifiautologin.sh
+++ wifiautologin.sh
@@ -5,8 +5,8 @@
 
 username="这里输入账号"
 password="这里输入密码"
-channelshow="%E4%B8%AD%E5%9B%BD%E7%A7%BB%E5%8A%A8"  #中国移动
-#channelshow="%E4%B8%AD%E5%9B%BD%E7%94%B5%E4%BF%A1" #中国电信
+channelshow="%E4%B8%AD%E5%9B%BD%E7%A7%BB%E5%8A%A8&channel=%40cmcc"     #中国移动
+#channelshow="%E4%B8%AD%E5%9B%BD%E7%94%B5%E4%BF%A1&channel=%40telecom" #中国电信
 while :
 do
     if [ $(ping -c 3 -i 3 www.baidu.com| grep -c ttl)  -eq 0 ];
@@ -16,7 +16,7 @@ do
             EXEC=$(grep "name=\"execution\"" /tmp |head -n 1 | awk -F \" '{print $ 6}')
             INSERTCOOKIE=$(grep "insert_cookie" cookie|awk '{print $7}')
             JSESSIONID=$(grep "JSESSIONID" cookie |awk '{print$7}')
-            DATA="username="$username"&password="$password"&channelshow="$channelshow"&channel=%40cmcc&lt="$LT"&execution="$EXEC"&_eventId=submit&login=%E7%99%BB%E5%BD%95"
+            DATA="username="$username"&password="$password"&channelshow="$channelshow"&lt="$LT"&execution="$EXEC"&_eventId=submit&login=%E7%99%BB%E5%BD%95"
             DATA2="Cookie: JSESSIONID="$JSESSIONID"; insert_cookie="$INSERTCOOKIE
             curl 'https://u.njtech.edu.cn/cas/login?service=https%3A%2F%2Fu.njtech.edu.cn%2Foauth2%2Fauthorize%3Fclient_id%3DOe7wtp9CAMW0FVygUasZ%26response_type%3Dcode%26state%3Dnjtech' -H 'Accept: */*' -H 'Accept-Language: zh-cn' -H 'User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' -H "$DATA2"  --data "$DATA" --compressed ;
     fi
